d38a76b5b9310c410a564438602e3b9037033eb8
Yahya Arhoune <yahya.arhoune@gmail.com>
2023-03-17 10:01:51 -0700

    TASK 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,22 +4,21 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
- * main - main function 
+ * main - determine if a number is positive, or negative or zero.
  *
- * Return: always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	        printf("%d is positive\n", n);
-	else if (n == 0)
-                printf("%d is zero\n", n);
-	else
-	        printf("%d is negative\n", n);
-	return (0);	 
+int n;	
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+	printf("%d is positive\n", n);
+else if (n == 0)
+        printf("%d is zero\n", n)
+else  
+	printf("%d is negative\n", n);
+return (0);	 
 }
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes here */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Print the last degits of a randomly generated number.
+ * and wheter is greater than 5, less than 6 or 0.
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int n;
+	
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if ((n % 10) > 5)
+{
+	ptintf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+}
+else if ((n % 10) < 6 && (n % 10) != 0)
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+}
+else 
+{
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
+}
+return(0);
+}
+
