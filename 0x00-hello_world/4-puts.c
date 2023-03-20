2f2ee744d987c3a329a3ac54f4a98ef8a6d8e53a
Yahya Arhoune <yahya.arhoune@gmail.com>
2023-03-20 11:04:50 -0700

    HELLO WORLD

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -8,5 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
