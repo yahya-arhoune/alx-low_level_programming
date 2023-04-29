268512bf249031fb290a6948fa92166a0e870a3f
Yahya Arhoune <yahya.arhoune@gmail.com>
2023-04-29 09:36:16 -0700

    updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (current == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(intlist_t));
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
